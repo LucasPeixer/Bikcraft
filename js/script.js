05be167895080b5843c8f4118f72d69e8a8f5d5f
Lucas Peixer <Lucaspeicher@hotmail.com>
2024-07-10 11:53:00 -0300

    feat: adiciona ações da galeria de fotos

--- js/script.js
+++ js/script.js
@@ -46,4 +46,20 @@ function eventosPerguntas(pergunta) {
 
 perguntas.forEach(eventosPerguntas);
 
-console.log(perguntas);
+// Galeria de Bicicletas
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+function trocarImagem(e) {
+  const img = e.currentTarget;
+  const media = matchMedia("(min-width: 1000px)").matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
